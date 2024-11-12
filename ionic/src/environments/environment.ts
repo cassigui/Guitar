ea9133f0fcd4f48db3b2d0eb6bb34ac6870f3fef
cassigui <caguilherme14@gmail.com>
2024-11-12 10:30:31 -0300

    projeto dinamico

---
+++ ionic/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    api_url: 'https://guitarwerock.wf',
+    url_s3: 'https://guitar-treinamento.s3.sa-east-1.amazonaws.com/',
+    api_token: 'guitar_dev_token',
+};
